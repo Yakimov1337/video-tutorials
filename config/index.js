b49e537214907ea46149202533e143f2874eecb0
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-30 01:04:31 +0300

    Change name

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT:3000,
-    DB_CONNECTION_STRING: 'mongodb://localhost:27017/realExam',
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/video-tutorials',
     TOKEN_SECRET: 'secured',
     COOKIE_NAME: 'SESSION9'
 }
