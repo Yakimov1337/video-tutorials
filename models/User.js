ae9bb0ee6df87299f607f12520da07c63107ee2d
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-30 04:20:56 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose');
 const schema = new Schema ({
     //TODO adapt parameters to project requirements
     username: { type: String, required: true},
-    hashedPassword: { type: String, required: true}
+    hashedPassword: { type: String, required: true},
+    enrolledCourses: [{ type: Schema.Types.ObjectId, ref: 'Course', default: [] }]
 })
 
 module.exports = model('User',schema);
