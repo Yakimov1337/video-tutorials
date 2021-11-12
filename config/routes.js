1ec0b49fcf7ca650a88e003d4dacdb7acb668587
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-11-12 04:20:09 +0200

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,13 @@
 const authController = require('../controllers/authController');
 const homeController = require('../controllers/homeController');
 const courseController = require('../controllers/courseController');
+const userController = require('../controllers/userController.js');
+
 
 
 module.exports = (app) => {
     app.use('/', homeController),
     app.use('/auth', authController),
     app.use('/course', courseController)
-
+    app.use('/user', userController)
 };
